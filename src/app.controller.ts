069a778de37ad8a832f6e2c8d24b6d75ee364ff3
kcode_tech <kos.ter.code@gmail.com>
2025-09-07 09:33:37 +0300

    sse-example-done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Logger, Sse, MessageEvent, Req } from '@nestjs/common';
+import { interval, map, Observable, Subject, takeUntil, tap } from 'rxjs';
 
-@Controller()
+@Controller('event')
 export class AppController {
-  constructor() {}
+  @Sse('timer')
+  addTimer(@Req() request): Observable<MessageEvent> {
+    let cleanup$ = new Subject<void>();
+    const invrl = interval(1000).pipe(
+      takeUntil(cleanup$),
+      map(() => ({
+        data: {
+          time: new Date().toISOString(),
+        },
+        type: 'time-change',
+      })),
+      tap((e: any) => {
+        Logger.log(e.data, 'SSE:SENT');
+      }),
+    );
+
+    Logger.debug('CONNECTED', 'SSE:STATE');
+
+    request.on('close', () => {
+      if (cleanup$) {
+        cleanup$.next();
+        cleanup$.complete();
+        (cleanup$ as any) = null;
+        Logger.debug('DISCONNECTED', 'SSE:STATE');
+      }
+    });
+
+    return invrl;
+  }
 }
